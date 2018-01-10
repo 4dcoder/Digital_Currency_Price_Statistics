193136e70dc9f4d01af6568a08607ed2191eb7b7
linan-zhou <zhoulinan14@gmail.com>
2018-01-10 22:20:37 +0800

    Market comparison of BTC/USDT between huobi and okcoin.Keep the latest data in one hour in the database.
    Signed-off-by: linan-zhou <zhoulinan14@gmail.com>

--- allkline_by_echarts/savedata.py
+++ allkline_by_echarts/savedata.py
@@ -37,8 +37,8 @@ def deldataokcoin(endtime):
     endtime1 = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(endtime))
     cur.execute('''DELETE FROM okcoin WHERE id < %s''', endtime1)
 
-urlhb = '''https://api.huobi.pro/market/history/kline?period=1min&size=60&symbol=btcusdt'''
-urlokcoin = '''https://www.okcoin.com/api/v1/kline.do?symbol=btc_usd&type=1min&size=60'''
+urlhb = '''https://api.huobi.pro/market/history/kline?period=1min&size=1&symbol=btcusdt'''
+urlokcoin = '''https://www.okcoin.com/api/v1/kline.do?symbol=btc_usd&type=1min&size=1'''
 headershb = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36'
 }
@@ -52,8 +52,8 @@ while True:
     j = json.loads(wb_data_okcoin.text)
     j = sorted(j, key=lambda x: x[0])
     savedataokcoin(j)
-    deldatahb(time.time()-60)
-    deldataokcoin(time.time()-60)
+    deldatahb(time.time()-3600)
+    deldataokcoin(time.time()-3600)
     time.sleep(60)
 
 cur.close()
