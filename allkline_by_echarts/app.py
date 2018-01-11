0f44bcf8b93b5fbea4eaf6bf5ad74f7f865773fd
linan-zhou <zhoulinan14@gmail.com>
2018-01-11 18:20:56 +0800

    Dynamic market comparison (Update every minute) of BTC/USDT between huobi and okcoin.( the latest data in one hour is saved in the database.)
    Signed-off-by: linan-zhou <zhoulinan14@gmail.com>

--- allkline_by_echarts/app.py
+++ allkline_by_echarts/app.py
@@ -4,16 +4,16 @@ import pymysql
 
 app = Flask(__name__)
 
-conn = pymysql.connect(host='localhost', user='root', passwd='', db='mysql', charset='utf8')
-cur = conn.cursor()
-cur.execute("USE DIGICCY")
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
-@app.route('/5minutesaverage', methods=['GET'])
+@app.route('/5minutesaverage', methods=['GET','POST'])
 def cal_5_minutes():
+    conn = pymysql.connect(host='localhost', user='root', passwd='', db='mysql', charset='utf8')
+    cur = conn.cursor()
+    cur.execute("USE DIGICCY")
     cur.execute("SELECT id, close FROM hb")
     res = cur.fetchall()
     tot = 0
@@ -44,6 +44,8 @@ def cal_5_minutes():
             closeaver5_okcoin.append((now / 5))
         tot += 1
         now -= res[tot - 4][1]
+    cur.close()
+    conn.close()
     return jsonify(time=mmnt, aver_hb=closeaver5_hb, aver_okcoin=closeaver5_okcoin)
 
 
